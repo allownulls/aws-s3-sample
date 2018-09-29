6cd8ec012e4eda4b2d60df9896cb251fb3f7d43f
Sergey Drobyshev <sdrobyshev@gmail.com>
2018-09-29 03:10:46 +0200

    Initial

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+
+
+namespace AmazonS3Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task.Run(async () =>
+            {
+                await S3Sample.Test();
+            }).GetAwaiter().GetResult();
+                      
+        }
+    }
+}
+
+
+
---
+++ readme.md
@@ -0,0 +1,5 @@
+### Amazon s3 storage accessing example (client-side encryption)
+
+Fixed for .net core 2:
+- all SDK calls are async
+- fixed ToXmlString "operation is not supported on this platform"
